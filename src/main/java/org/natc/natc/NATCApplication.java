4e649666c2f9225aff9e88a988b4c9ccfa30bb2f
Ed Hewell <ed_hewell@gap.com>
2020-02-24 15:16:58 -0800

    Initial project commit

---
+++ src/main/java/org/natc/natc/NATCApplication.java
@@ -0,0 +1,13 @@
+package org.natc.natc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NATCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NATCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/natc/natc/NATCApplicationTests.java
@@ -0,0 +1,13 @@
+package org.natc.natc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NATCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
