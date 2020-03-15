52660f6605316823068f8557bf0e3e220a8cec97
Ed Hewell <ed_hewell@gap.com>
2020-03-14 22:29:36 -0700

    Implementing player search service integration tests

--- src/test/integration/java/org/natc/natc/service/ManagerSearchServiceIntegrationTest.java
+++ src/test/integration/java/org/natc/natc/service/ManagerSearchServiceIntegrationTest.java
@@ -13,8 +13,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 class ManagerSearchServiceIntegrationTest extends NATCServiceIntegrationTest {
 
